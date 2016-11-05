3a04358838d1bc1cf616d4b887bb86be2d41c2de
Kendsdavs <kendra.wieck@gmail.com>
2016-11-05 08:32:01 -0400

    complete crudl DAL and API

--- API/app.js
+++ API/app.js
@@ -1,23 +1,297 @@
-// Our API goes here!
-var HTTPError = require('node-http-error')
-var bodyParser = require('body-parser')
-var express = require('express')
-var app = express()
-const port = process.env.PORT || 3000  // Use process.env to acess the contents of the user environment
-const dal = require('../DAL/no-sql.js')
-
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
+const HTTPError = require('node-http-error');
+const dal = require('../DAL/no-sql.js');
+var bodyParser = require('body-parser');
 app.use(bodyParser.json())
 
-// ------  GENERAL  -------  //
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/', function(req, res) {
+    res.send('hello world')
 })
 
+app.get('/food/:id', function(req, res, next) {
+    const foodID = req.params.id;
+
+    dal.getFood(foodID, function(err, data) {
+        if (err) {
+            var responseError = BuildResponseError(err)
+            //console.log("Error calling dal.getReliefEffort", err)
+            return next(new HTTPError(responseError.status, responseError.message))
+        }
+        if (data) {
+            res.append('Content-type', 'application/json')
+            res.send(data);
+        }
+    })
+})
+
+app.get('/food', function(req, res, next) {
+    const sortByParam = req.query.sortby || 'food';
+    //const sortBy = getReliefEffortSortBy(sortByParam, dalModule);
+    const sortBy = sortByParam
+    const sortToken = req.query.sorttoken || '';
+    const limit = req.query.limit || 5;
+
+    dal.listFood(sortBy, sortToken, limit, function callback(err, data) {
+        if (err) {
+            var responseError = BuildResponseError(err)
+            return next(new HTTPError(responseError.status, responseError.message))
+        }
+        if (data) {
+            console.log("HERE is the list from " + req.path, data)
+            res.append('Content-type', 'application/json')
+            res.status(200).send(data)
+        }
+    })
+})
+
+app.get('/beverage/:id', function(req, res) {
+    const beverageID = req.params.id;
+
+    dal.getFood(beverageID, function(err, data) {
+        if (err) {
+            var responseError = BuildResponseError(err)
+            //console.log("Error calling dal.getReliefEffort", err)
+            return next(new HTTPError(responseError.status, responseError.message))
+        }
+        if (data) {
+            res.append('Content-type', 'application/json')
+            res.send(data);
+        }
+    })
+})
+
+app.get('/beverage', function(req, res, next) {
+    const sortByParam = req.query.sortby || 'beverage';
+    const sortBy = sortByParam;
+    const sortToken = req.query.sorttoken || '';
+    const limit = req.query.limit || 5;
+
+    dal.listBeverage(sortBy, sortToken, limit, function callback(err, data) {
+        if (err) {
+            var responseError = BuildResponseError(err)
+            return next(new HTTPError(responseError.status, responseError.message))
+        }
+        if (data) {
+            console.log("HERE is the list from " + req.path, data)
+            res.append('Content-type', 'application/json')
+            res.status(200).send(data)
+        }
+    })
+})
+
+app.put('/food/:id', function(req, res, next) {
+    const foodID = req.params.id;
+
+    dal.getFood(foodID, function callback(err, data) {
+        if (err) {
+            var responseError = BuildResponseError(err)
+            return next(new HTTPError(responseError.status, responseError.message))
+        }
+        if (data) {
+            //data["name"] !== req.body["name"]
+            dal.updateFood(req.body, function callback(updateerror, updateddata) {
+                if (updateerror) {
+                    var responseError = BuildResponseError(err)
+                    return next(new HTTPError(responseError.status, responseError.message))
+                }
+                if (updateddata) {
+                    updateddata.body["name"]
+                    console.log('You updated ', updateddata)
+                    res.append('Content-type', 'application/json')
+                    res.status(200).send(updateddata)
+                }
+            })
+        }
+    })
+})
+
+app.get('/ingredients/:id', function(req, res, next) {
+    dal.queryIngredients(req.params.id, function(err, data) {
+        if (err) {
+            var responseError = BuildResponseError(err)
+            return next(new HTTPError(responseError.status, responseError.message))
+        }
+        if (data) {
+            console.log('You updated ', data)
+            res.append('Content-type', 'application/json')
+            res.status(200).send(data)
+        }
+    })
+})
+
+app.get('/subtype/:id', function(req, res, next) {
+    dal.querySubtype(req.params.id, function(err, data) {
+        if (err) {
+            var responseError = BuildResponseError(err)
+            return next(new HTTPError(responseError.status, responseError.message))
+        }
+        if (data) {
+            console.log('Here are your subtype recipies ', data)
+            res.append('Content-type', 'application/json')
+            res.status(200).send(data)
+        }
+    })
+})
+
+app.put('/beverage/:id', function(req, res, next) {
+    dal.getBeverage(req.params.id, function(err, data) {
+        if (err) {
+            var responseError = BuildResponseError(err)
+            return next(new HTTPError(responseError.status, responseError.message))
+        }
+        if (data)
+        dal.updateBeverage(req.body, function callback(updatederror, updateddata) {
+            if (updatederror) {
+                var responseError = BuildResponseError(err)
+                return next(new HTTPError(responseError.status, responseError.message))
+            }
+            if (updateddata) {
+                console.log("You updated a beverage " + updateddata)
+                res.append('Content-type', 'application/json')
+                res.status(200).send(updateddata)
+            }
+        })
+
+    })
+})
+
+app.post('/beverage', function(req, res, next) {
+    console.log(req.body)
+    dal.createBeverage(req.body, function callback(err, data) {
+        if (err) {
+            var responseError = BuildResponseError(err)
+            return next(new HTTPError(responseError.status, responseError.message))
+        }
+        if (data) {
+            //req.body["name"] = data["name"]
+            console.log("You created the beverage ", req.body["name"])
+            res.append('Content-type', 'application/json')
+            res.status(200).send(data)
+        }
+    })
+
+})
+
+app.post('/food', function(req, res, next) {
+    //console.log(req.body)
+    dal.createFood(req.body, function callback(err, data) {
+        if(err) {
+            var responseError = BuildResponseError(err)
+            return next(new HTTPError(responseError.status, responseError.message))
+        }
+        if(data) {
+            console.log("You created the food", req.body["name"])
+            res.append('Content-type', 'application/json')
+            res.status(200).send(data)
+        }
+    })
+})
+
+app.delete('/food/:id', function(req, res, next) {
+    dal.getFood(req.params.id, function callback(err, data) {
+        if (err) {
+            var responseError = BuildResponseError(err)
+            return next(new HTTPError(responseError.status, responseError.message))
+        }
+        if (data)
+            dal.deleteFood(data, function callback(deletederror, deleteddata) {
+            if (deletederror) {
+                var responseError = BuildResponseError(err)
+                //console.log("Error calling dal.getReliefEffort", err)
+                return next(new HTTPError(responseError.status, responseError.message))
+            }
+            if (deleteddata) {
+              console.log("DELETE " + req.path, deleteddata)
+              res.append('Content-type', 'application/json')
+              res.status(200).send(deleteddata)
+            }
+        })
+    })
+})
+
+app.delete('/beverage/:id', function(req, res, next) {
+    dal.getBeverage(req.params.id, function callback(err, data) {
+        if (err) {
+            var responseError = BuildResponseError(err)
+            return next(new HTTPError(responseError.status, responseError.message))
+        }
+        if (data)
+        dal.deleteBeverage(data, function callback(deletederror, deleteddata) {
+        if (deletederror) {
+            var responseError = BuildResponseError(err)
+            //console.log("Error calling dal.getReliefEffort", err)
+            return next(new HTTPError(responseError.status, responseError.message))
+        }
+        if (deleteddata) {
+          console.log("DELETE " + req.path, deleteddata)
+          res.append('Content-type', 'application/json')
+          res.status(200).send(deleteddata)
+        }
+    })
+    })
+})
+
+function BuildResponseError(err) {
+
+    // no sql error message example
+    //     { id: 'person_jackiekennedyo1922@gmail.org',
+    // error: 'conflict',
+    // reason: 'Document update conflict.',
+    // name: 'conflict',
+    // status: 409,
+    // message: 'Document update conflict.',
+    // ok: true }
+    //
+    // // custom DAL validation message example
+    //
+    // {
+    // error: 'Bad Request',
+    // reason: 'Unnecessary _id property within data.'
+    // name: 'Bad Request',
+    // status: 400,
+    // message: 'Unnecessary _id property within data.',
+    // ok: true }
+
+    // if the first three characters are a number then return the error code otherwise
+    //  default to 400 (bad request)
+    const statuscheck = isNaN(err.message.substring(0, 3)) === true
+        ? "400"
+        : err.message.substring(0, 3)
+    const status = err.status
+        ? Number(err.status)
+        : Number(statuscheck)
+    const message = err.status
+        ? err.message
+        : err.message.substring(3)
+    const reason = message
+    const error = status === 400
+        ? "Bad Request"
+        : err.name
+    const name = error
+
+    var errormsg = {}
+    errormsg.error = error
+    errormsg.reason = reason
+    errormsg.name = name
+    errormsg.status = status
+    errormsg.message = message
+
+    //   { error: 'Bad Request',
+    // reason: 'Missing email property within data',
+    // name: 'Bad Request',
+    // status: 400,
+    // message: 'Missing email property within data' }
+    console.log("BuildResponseError-->", errormsg)
+    return errormsg
+}
+// middleware for errors
 app.use(function(err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).send(err)
+    console.log(req.method, " ", req.path, " err: ", err)
+    res.status(err.status || 500)
+    res.send(err)
 })
-
-app.listen(port, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
+});
--- DAL/no-sql.js
+++ DAL/no-sql.js
@@ -1,12 +1,10 @@
 /*jshint esversion: 6 */
 const path = require('path');
-const PouchDB = require('pouchdb-http');
+const PouchDB = require('pouchdb-http'); //adapter
 PouchDB.plugin(require('pouchdb-mapreduce'));
-const fetchConfig = require('zero-config');
+const fetchConfig = require('zero-config'); // this and the next line are connected
 
-var config = fetchConfig(path.join(__dirname, '..'), {
-    dcValue: 'test'
-});
+var config = fetchConfig(path.join(__dirname, '..'), {dcValue: 'test'});
 const urlFormat = require('url').format;
 const db = new PouchDB(urlFormat(config.get("couch")));
 
@@ -21,17 +19,23 @@ var dal = {
     updateFood: updateFood,
     createFood: createFood,
     deleteFood: deleteFood,
-    listFood: listFood
+    listFood: listFood,
+    queryIngredients: queryIngredients,
+    querySubtype: querySubtype
 };
 
+
 //  ---------  UTILITY  ----------  //
+
 function queryDB(sortBy, startKey, limit, callback) {
     if (typeof startKey == "undefined" || startKey === null) {
         return callback(new Error('Missing search parameter'));
     } else if (typeof limit == "undefined" || limit === null || limit === 0) {
         return callback(new Error('Missing limit parameter'));
     } else {
-        limit = startKey === '' ? Number(limit) : Number(limit) + 1;
+        limit = startKey === ''
+            ? Number(limit)
+            : Number(limit) + 1;
         console.log("sortBy: ", sortBy, " startKey: ", startKey, " limit: ", limit)
 
         db.query(sortBy, {
@@ -39,8 +43,10 @@ function queryDB(sortBy, startKey, limit, callback) {
             limit: limit,
             include_docs: true
         }).then(function(result) {
-            if (startKey !== '' && result.rows.length > 0) { result.rows.shift(); }
-            return callback(null, result.rows.map(convertPersons));
+            if (startKey !== '' && result.rows.length > 0) {
+                result.rows.shift();
+            }
+            return callback(null, result.rows);
         }).catch(function(err) {
             return callback(err);
         });
@@ -53,9 +59,12 @@ function getDocByID(id, callback) {
         return callback(new Error('400Missing id parameter'));
     } else {
         db.get(id, function(err, data) {
-            if (err) return callback(err);
-            if (data) return callback(null, data);
-        });
+            if (err)
+                return callback(err);
+            if (data)
+                return callback(null, data);
+            }
+        );
     }
 }
 
@@ -64,9 +73,12 @@ function createView(designDoc, callback) {
         return callback(new Error('400Missing design document.'));
     } else {
         db.put(designDoc, function(err, response) {
-            if (err) return callback(err);
-            if (response) return callback(null, response);
-        });
+            if (err)
+                return callback(err);
+            if (response)
+                return callback(null, response);
+            }
+        );
     }
 }
 
@@ -80,9 +92,12 @@ function updateDoc(data, callback) {
         return callback(new Error('400Missing rev property from data'));
     } else {
         db.put(data, function(err, response) {
-            if (err) return callback(err);
-            if (response) return callback(null, response);
-        });
+            if (err)
+                return callback(err);
+            if (response)
+                return callback(null, response);
+            }
+        );
     }
 }
 
@@ -95,20 +110,125 @@ function deleteDoc(data, callback) {
         return callback(new Error('400Missing _rev property from data'));
     } else {
         db.remove(data, function(err, response) {
-            if (err) return callback(err);
-            if (response) return callback(null, response);
-        });
+            if (err)
+                return callback(err);
+            if (response)
+                return callback(null, response);
+            }
+        );
     }
 }
 
 //  -----------  BEVERAGES  ------------  //
-function getBeverage() {
-  
+function getBeverage(id, callback) {
+    getDocByID(id, callback)
+}
+function updateBeverage(data, callback) {
+    updateDoc(data, callback);
+}
+
+function deleteBeverage(data, callback) {
+    deleteDoc(data, callback);
+}
+function listBeverage(sortBy, startKey, limit, callback) {
+    queryDB(sortBy, startKey, limit, callback)
+}
+function createBeverage(data, callback) {
+    if (data.hasOwnProperty('name') !== true) {
+        return callback(new Error('400Missing name property within data'));
+    }
+    if (data.hasOwnProperty('subtype') !== true) {
+        return callback(new Error('400Missing subtype property within data'));
+    }
+    if (data.hasOwnProperty('ingredients') !== true) {
+        return callback(new Error('400Missing ingredents property within data'));
+    }
+    if (data.hasOwnProperty('_id') === true) {
+        return callback(new Error('400 ID is not allowed within data'));
+    }
+    if (data.hasOwnProperty('_rev') === true) {
+        return callback(new Error('400 Rev is not allowed within data'));
+    }
+
+    data._id = 'beverage_' + data.name
+    data.type = 'beverage'
+
+    db.put(data, function(err, response) {
+        if (err) {
+            console.log(err)
+            return callback(err)
+        }
+        console.log("you created a beverage" + response)
+        return callback(null, response)
+    })
 }
 
 //  -----------  FOOD  --------------  ///
 function getFood(id, callback) {
-  getDocByID(id, callback)
+    getDocByID(id, callback)
 }
+function updateFood(data, callback) {
+    updateDoc(data, callback);
+}
+
+function deleteFood(data, callback) {
+    deleteDoc(data, callback);
+}
+
+function createFood(data, callback) {
+    if (data.hasOwnProperty('name') !== true) {
+        return callback(new Error('400Missing name property within data'));
+    }
+    if (data.hasOwnProperty('subtype') !== true) {
+        return callback(new Error('400Missing subtype property within data'));
+    }
+    if (data.hasOwnProperty('ingredients') !== true) {
+        return callback(new Error('400Missing ingredents property within data'));
+    }
+    if (data.hasOwnProperty('_id') === true) {
+        return callback(new Error('400 ID is not allowed within data'));
+    }
+    if (data.hasOwnProperty('_rev') === true) {
+        return callback(new Error('400 Rev is not allowed within data'));
+    }
+
+    data._id = 'food_' + data.name
+    data.type = 'food'
+
+    db.put(data, function(err, response) {
+        if (err) {
+            console.log(err)
+            return callback(err)
+        }
+        console.log("you created a food" + response)
+        return callback(null, response)
+    })
+}
+
+function listFood(sortBy, startKey, limit, callback) {
+    queryDB(sortBy, startKey, limit, callback)
+}
+function queryIngredients(ing, callback) {
+        db.query('ingredients', {
+            startkey: ing,
+            endkey: ing + "\uffff",
+            include_docs: true
+        }, function (err, data) {
+            if (err) return callback(err)
+            callback(null, data)
+        })
+    }
+
+function querySubtype(subt, callback) {
+        db.query('subtype', {
+            startkey: subt,
+            endkey: subt + "\uffff",
+            include_docs: true
+        }, function (err, data) {
+            if (err) return callback(err)
+            callback(null, data)
+        })
+    }
+
 
 module.exports = dal
